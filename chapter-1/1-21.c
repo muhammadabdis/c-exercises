decb31d7429f06b1489c138a0a89979ef0fb5504
Muhammmad Abdis <abdissalim0@gmail.com>
2022-03-18 22:19:07 +0700

    Simplify 1-21 implementation

--- chapter-1/1-21.c
+++ chapter-1/1-21.c
@@ -1,5 +1,5 @@
 /*
-    Write a program entab that replaces strings of blanks by the
+    Exercise 1-21. Write a program entab that replaces strings of blanks by the
     minimum number of tabs and blanks to achieve the same spacing. Use the
     same tab stops as for detab. When either a tab or a single blank would suffice
     to reach a tab stop, which should be given preference?
@@ -29,8 +29,8 @@ int main() {
             // print remaining space
             for (int i = 0; i < ns; ++i)  {
                 putchar(' ');
-                ++pos;
             }
+            pos = pos + ns;
             ns = 0;
             
             putchar(c);
