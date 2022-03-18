c61c5980296b22e8d7f279d6dca1190196d619d6
Muhammmad Abdis <abdissalim0@gmail.com>
2022-03-18 22:07:02 +0700

    Rename 1-21.c to 1-20.c

---
+++ chapter-1/1-20.c
@@ -0,0 +1,32 @@
+/*
+    Exercise 1-20. Write a program detab that replaces tabs in the input with the
+    proper number of blanks to space to the next tab stop. Assume a fixed set of
+    tab stops, say every n columns. Should n be a variable or a symbolic parameter?
+*/
+
+#include <stdio.h>
+
+#define TAB 8
+
+int main() {
+    int col = 0;
+    int c;
+
+    while((c = getchar()) != EOF) {
+        if (c == '\t') {
+            int tabstop = TAB - (col % TAB);
+
+            for (int i = 0; i < tabstop; ++i) {
+                putchar(' ');
+                ++col;
+            }
+        } else if (c == '\n') {
+            putchar(c);
+        } else {
+            putchar(c);
+            ++col;
+        }
+    }
+
+    return 0;
+}
