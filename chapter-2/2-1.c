b333e8cd075ae8550be0e59027ed0c020e0e1704
Muhammad Abdis <abdissalim0@gmail.com>
2023-07-24 22:00:50 +0700

    feat: 2-1

---
+++ chapter-2/2-1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <limits.h>
+
+int main() {
+    printf("Range of char: %d -> %d\n", CHAR_MIN, CHAR_MAX);
+    printf("Range of int: %d -> %d\n", INT_MIN, INT_MAX);
+    printf("Range of short: %d -> %d\n", SHRT_MIN, SHRT_MAX);
+    printf("Range of long: %ld -> %ld\n", LONG_MIN, LONG_MAX);
+    printf("Range of signed char: %d -> %d\n", SCHAR_MIN, SCHAR_MAX);
+    printf("Range of unsigned char: %u -> %u\n", 0, UCHAR_MAX);
+    printf("Range of unsigned int: %u -> %u\n", 0, UINT_MAX);
+    printf("Range of unsigned short: %u -> %u\n", 0, USHRT_MAX);
+    printf("Range of unsigned long: %u -> %lu\n", 0, ULONG_MAX);
+}
