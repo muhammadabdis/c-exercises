d759078baddf46f210c204061dfb1a0f37a4518e
Muhammad Abdis <abdissalim0@gmail.com>
2023-10-17 13:09:19 +0700

    feat(2-6): add exercise description

--- chapter-2/2-6.c
+++ chapter-2/2-6.c
@@ -1,13 +1,19 @@
+/*
+    Exercise 2-6. Write a function setbits(x,p,n,y) that returns x with the n
+    bits that begin at position p set to the rightmost n bits of y, leaving the other
+    bits unchanged.
+*/
+
 #include <stdio.h>
 
 unsigned setbits(unsigned x, int p, int n, unsigned y);
 
 int main()
 {
-  printf("setbits result: %d", setbits(18, 3, 2, 22));
+    printf("setbits result: %d", setbits(18, 3, 2, 22));
 }
 
 unsigned setbits(unsigned x, int p, int n, unsigned y)
 {
-  return (x & ~(~(~0 << n) << (p + 1 - n))) | ((y & ~(~0 << n)) << (p + 1 - n));
+    return (x & ~(~(~0 << n) << (p + 1 - n))) | ((y & ~(~0 << n)) << (p + 1 - n));
 }
