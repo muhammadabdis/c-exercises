20d06df1dee2e8b52b09c610e1d9dad4433ddbc7
Muhammad Abdis <abdissalim0@gmail.com>
2023-10-21 11:07:52 +0700

    feat: 3-1

---
+++ chapter-3/3-1.c
@@ -0,0 +1,43 @@
+/*
+    Exercise 3-1. Our binary search makes two tests inside the loop, when one
+    would suffice (at the price of more tests outside). Write a version with only one
+    test inside the loop and measure the difference in run-time
+*/
+
+#include <stdio.h>
+
+int binsearch(int x, int v[], int n);
+
+int main()
+{
+    int arr[] = {4, 5, 6, 7, 8};
+
+    int i = binsearch(8, arr, 5);
+
+    printf("found index: %d", i);
+
+    return 0;
+}
+
+int binsearch(int x, int v[], int n)
+{
+    int low, high, mid;
+
+    low = 0;
+    high = n - 1;
+
+    while (low <= high)
+    {
+        mid = (low + high) / 2;
+
+        if (x > v[mid])
+            low = mid + 1;
+        else
+            high = mid - 1;
+    }
+
+    if (x == v[low])
+        return low;
+    else
+        return -1;
+}
