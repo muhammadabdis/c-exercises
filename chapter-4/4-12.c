8edc910285132c79b343893bddc3a5a01496510b
Muhammad Abdis <abdissalim0@gmail.com>
2023-11-26 02:07:21 +0700

    feat: 4-12

---
+++ chapter-4/4-12.c
@@ -0,0 +1,34 @@
+/*
+    Exercise 4-12. Adapt the ideas of printd to write a recursive version of itoa;
+    that is, convert an integer into a string by calling a recursive routine.
+*/
+
+#include <stdio.h>
+
+#define MAXLEN 1000
+
+void itoa(char s[], int n);
+
+int main()
+{
+    char s[MAXLEN];
+
+    itoa(s, 9812);
+
+    printf("itoa: %s\n", s);
+}
+
+void itoa(char s[], int n)
+{
+    static int i;
+
+    if (n == 0) {
+        i = 0;
+        return;
+    }
+
+    itoa(s, n / 10);
+
+    s[i++] = n % 10 + '0';
+    s[i] = '\0';
+}
