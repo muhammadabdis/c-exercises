73d1fbe696f2fa16d891e9095dd240ced719a521
Muhammad Abdis <abdissalim0@gmail.com>
2023-12-01 13:32:39 +0700

    feat: 4-14

---
+++ chapter-4/4-14.c
@@ -0,0 +1,24 @@
+/*
+    Exercise 4-14. Define a macro swap(t,x,y) that interchanges two arguments
+    of type t. (Block structure will help.)
+*/
+
+#include <stdio.h>
+
+#define swap(t, x, y) t z = x; \
+    x = y; \
+    y = z;
+
+#define dprint(exp) printf(#exp " = %c\n", exp);
+
+int main() {
+    char a = 'a';
+    char b = 'b';
+
+    swap(char, a, b);
+
+    dprint(a);
+    dprint(b);
+
+    return 0;
+}
