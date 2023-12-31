a4a71926306ef80318ca7752eebc66b64aca6573
Muhammad Abdis <abdissalim0@gmail.com>
2023-12-31 17:20:19 +0700

    feat(5-1): add description

--- chapter-5/5-1.c
+++ chapter-5/5-1.c
@@ -1,3 +1,8 @@
+/*
+    Exercise 5-l. As written, getint treats a + or - not followed by a digit as a
+    valid representation of zero. Fix it to push such a character back on the input.
+*/
+
 #include <stdio.h>
 #include <ctype.h>
 
