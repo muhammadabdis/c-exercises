e2c15bf979e8d9082cadda3128609847d31a08be
Muhammad Abdis <abdissalim0@gmail.com>
2024-03-30 06:22:53 +0700

    fix(5-11): entab description

--- chapter-5/5-11_entab.c
+++ chapter-5/5-11_entab.c
@@ -1,7 +1,7 @@
 /*
  * Exercise 5-11. Modify the programs entab and detab (written as exercises in
  * Chapter 1) to accept a list of tab stops as arguments. Use the default tab set-
- * (for the user) default behaviour.
+ * tings if there are no arguments.
  */
 
 #include <stdio.h>
