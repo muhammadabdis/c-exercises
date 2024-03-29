dd2ec0bb9d5da2484b7eb96351fd1e4e6bba4f14
Muhammad Abdis <abdissalim0@gmail.com>
2024-03-30 06:12:16 +0700

    feat(5-11): detab

---
+++ chapter-5/5-11_detab.c
@@ -0,0 +1,38 @@
+/*
+ * Exercise 5-11. Modify the programs entab and detab (written as exercises in
+ * Chapter 1) to accept a list of tab stops as arguments. Use the default tab set-
+ * tings if there are no arguments.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX_TABSTOP 1000
+
+int tabstops[MAX_TABSTOP];
+int tp;
+
+int main(int argc, char *argv[])
+{
+    int col, c, i, j;
+    
+    col = 0; i = 0;
+    while (--argc > 0)
+	tabstops[i++] = atoi(*++argv);
+    while ((c = getchar()) != EOF) {
+        if (c == '\t') {
+            for (j = 0; j < tabstops[tp] - col; ++j) {
+                putchar(' ');
+                ++col;
+            }
+	    tp++;
+        } else if (c == '\n') {
+            putchar(c);
+	    tp = 0;
+	} else {
+            putchar(c);
+            ++col;
+        }
+    }
+    return 0;
+}
