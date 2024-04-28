54a61d590e611a97e813a8c278919ae99eb5ce12
Muhammad Abdis <abdissalim0@gmail.com>
2024-04-28 15:32:55 +0700

    fix: 5-12 missing description

--- chapter-5/5-12_entab.c
+++ chapter-5/5-12_entab.c
@@ -1,6 +1,8 @@
 /*
  * Exercise 5-12. Extend entab and detab to accept shorthand
  *      entab -m +n
+ * to mean tab stops every n columns, starting at column m. Choose convenient
+ * (for the user) default behavior.
  */
 
 #include <stdio.h>
